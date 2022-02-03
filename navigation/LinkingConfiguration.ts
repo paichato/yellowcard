9c6a4a767f8ff9cf45af811b96167d118bfc00e9
paichato <tsenane14@gmail.com>
2022-02-03 21:20:12 +0200

    updated names

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -15,14 +15,14 @@ const linking: LinkingOptions<RootStackParamList> = {
     screens: {
       Root: {
         screens: {
-          TabOne: {
+          Home: {
             screens: {
-              TabOneScreen: 'one',
+              Home: 'one',
             },
           },
-          TabTwo: {
+          MeuPerfil: {
             screens: {
-              TabTwoScreen: 'two',
+              MeuPerfil: 'two',
             },
           },
         },
