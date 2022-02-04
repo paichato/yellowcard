b6a17d6fc41624cfdc7a68a0254ed86908189ec8
paichato <tsenane14@gmail.com>
2022-02-05 00:57:44 +0200

    adjust styles for 2nd screen + like feature

--- screens/LigaDetalhes.tsx
+++ screens/LigaDetalhes.tsx
@@ -5,6 +5,8 @@ import { MaterialIcons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
 import http from '../api/http';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import fakedata from '../lib/fakedata';
+// import '../lib/fakedata.tsx'
 
 
 export default function LigaDetalhes({navigation,route}) {
@@ -12,11 +14,12 @@ export default function LigaDetalhes({navigation,route}) {
     const {league}=route.params;
 
     const [season,setSeason]=useState(2020);
-    const [leagueData,setLeagueData]=useState([]);
-    const [tableData,setTableData]=useState([]);
+    const [leagueData,setLeagueData]=useState(fakedata.response[0].league);
+    const [tableData,setTableData]=useState(fakedata.response[0].league.standings);
+    const [liked,setLiked]=useState(false);
 
     useEffect(()=>{
-        getLeagueDetails();
+        // getLeagueDetails();
     },[])
 
     const getLeagueDetails=()=>{
@@ -31,26 +34,31 @@ export default function LigaDetalhes({navigation,route}) {
         })
     }
 
+    const toggleLike=()=>{
+        setLiked(!liked);
+    }
+
     const TeamCard=({data,index})=>{
 
-        console.log(data[0]);
+        // console.log(data[0]);
         
         return(
             
-            <TouchableOpacity style={{flexDirection:'row', width:'100%', backgroundColor:'red', alignItems:'center'}}>
+            <TouchableOpacity style={styles.teamCardContainer}>
             <View>
                 <Image source={{uri:data?.team?.logo}} style={{width:50,height:50, borderRadius:10, overflow:'hidden'}} />
-                <View style={{backgroundColor:'white', borderRadius:5, alignItems:'center', width:'50%'}}>
+                <View style={{backgroundColor:'white', borderRadius:5, alignItems:'center', width:'50%', position:'absolute',top:-10, left:-10}}>
                     <Text style={{color:'black'}}>{data?.rank}</Text>
                 </View>
             </View>
-            <View style={{flexDirection:'row', width:'70%',}}>
-                <Text style={{color:'white',marginHorizontal:5}}>{data?.points}</Text>
-                <Text style={{color:'white',marginHorizontal:5}}>{data?.goalsDiff}</Text>
-                <Text style={{color:'white',marginHorizontal:5}}>{data?.all?.played}</Text>
-                <Text style={{color:'white',marginHorizontal:5}}>{data?.all?.win}</Text>
-                <Text style={{color:'white',marginHorizontal:5}}>{data?.all?.draw}</Text>
-                <Text style={{color:'white',marginHorizontal:5}}>{data?.all?.lose}</Text>
+            <View style={{flexDirection:'row', width:'70%',justifyContent:'space-around'}}>
+                <Text style={styles.textPoints}>{data?.points}</Text>
+                
+                <Text style={styles.textPoints}>{data?.all?.played}</Text>
+                <Text style={styles.textPoints}>{data?.all?.win}</Text>
+                <Text style={styles.textPoints}>{data?.all?.draw}</Text>
+                <Text style={styles.textPoints}>{data?.all?.lose}</Text>
+                <Text style={styles.textPoints}>{data?.goalsDiff}</Text>
             </View>
         </TouchableOpacity>
         )
@@ -64,22 +72,32 @@ export default function LigaDetalhes({navigation,route}) {
             </TouchableOpacity>
         
         <Text style={{color:Colors.dark.tint}}>{league?.name}</Text>
-        <TouchableOpacity>
-          <MaterialIcons name="favorite-outline" size={24} color={Colors.dark.primary} /> 
-          <MaterialIcons name="favorite" size={24} color={Colors.dark.primary} />     
+        <TouchableOpacity onPress={toggleLike}>
+          {!liked ?<MaterialIcons name="favorite-outline" size={24} color={Colors.dark.primary} /> 
+          :
+          <MaterialIcons name="favorite" size={24} color={Colors.dark.primary} />  }   
             </TouchableOpacity>
         
         </View>
 
-        <Image source={{uri:`${league.logo}`}} style={{width:150, height:150, borderRadius:10, overflow:'hidden', resizeMode:'contain'}} />
-        <View style={{width:'100%', flexDirection:'row', alignItems:'center',justifyContent:'space-between', borderBottomColor:Colors.dark.primary, borderBottomWidth:0.2, paddingBottom:20}}>
-            <TouchableOpacity style={{backgroundColor:Colors.dark.primary, alignItems:'center', justifyContent:'center', flexDirection:'row', padding:5}}>
+        <Image source={{uri:`${league.logo}`}} style={styles.bigLogo} />
+        <View style={styles.configBar}>
+            <TouchableOpacity style={styles.seasonButton}>
                 <Text >Escolher temporada</Text>
                 <MaterialIcons name="arrow-drop-down" size={24} color='black' />
                 </TouchableOpacity>
                 <Text style={{color:Colors.dark.tint}}>{leagueData?.season}</Text>
                 <Ionicons name="filter" size={24} color={Colors.dark.primary} />
         </View>
+        <View style={{flexDirection:'row', width:'70%',justifyContent:'space-around', marginLeft:'15%'}}>
+                <Text style={styles.textPointsBold}>P</Text>
+                
+                <Text style={styles.textPointsBold}>J</Text>
+                <Text style={styles.textPointsBold}>V</Text>
+                <Text style={styles.textPointsBold}>D</Text>
+                <Text style={styles.textPointsBold}>E</Text>
+                <Text style={styles.textPointsBold}>SG</Text>
+            </View>
 
         <View style={{height:hp('40%'),width:wp('100%')}}>
         <FlatList contentContainerStyle={{width: '100%',alignItems:'center', justifyContent:'space-between',}} 
@@ -99,5 +117,32 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'space-around',
       paddingHorizontal:10,
+      paddingTop:10,
     },
+    textPoints:{
+        color:'white',
+        marginHorizontal:5
+    },
+    textPointsBold:{
+        color:'white',
+        marginHorizontal:5,
+        fontWeight:'bold',
+    },
+    teamCardContainer:{
+        flexDirection:'row',
+         width:'100%',
+          backgroundColor:'#385e1333',
+           alignItems:'center',
+            marginBottom:10,
+             padding:10
+    },
+    bigLogo:{
+        width:150, height:150, borderRadius:10, overflow:'hidden', resizeMode:'contain'
+    },
+    seasonButton:{
+        backgroundColor:Colors.dark.primary, alignItems:'center', justifyContent:'center', flexDirection:'row', padding:5
+    },
+    configBar:{
+        width:'100%', flexDirection:'row', alignItems:'center',justifyContent:'space-between', borderBottomColor:Colors.dark.primary, borderBottomWidth:0.2, paddingBottom:20
+    }
 })
