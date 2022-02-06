96f046cfd433a4d162c707129ebf8c029464fe39
paichato <tsenane14@gmail.com>
2022-02-06 02:25:42 +0200

    added styles to Meu perfil

--- screens/MeuPerfil.tsx
+++ screens/MeuPerfil.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Colors from '../constants/Colors';
+
 
 export default function MeuPerfil() {
+
+
+  const random=[1,2,3,4,5,6]
+
+  const CustomButton=({text})=>{
+    return(
+<TouchableOpacity style={{backgroundColor:Colors.dark.primary, borderRadius:5,  alignItems:'center', justifyContent:'center', padding:10, margin:5}} >
+            <Text style={{color:Colors.dark.background}}>{text}</Text>
+          </TouchableOpacity>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/MeuPerfil.tsx" />
+      <Text style={styles.title}>Favoritos</Text>
+      <View  style={styles.wrappers}>
+          <Text>Ligas</Text>
+          <FlatList style={{width:'100%'}} keyboardShouldPersistTaps={'handled'} numColumns={3} horizontal={false} style={{flex:1}} 
+      contentContainerStyle={styles.leaguesList} data={random} renderItem={({item})=><CustomButton text='Serie A'/>} />
+          
+      </View>
+
+      <View  style={styles.wrappers}>
+          <Text>Equipas</Text>
+          <FlatList style={{width:'100%'}} keyboardShouldPersistTaps={'handled'} numColumns={3} horizontal={false} style={{flex:1}} 
+      contentContainerStyle={styles.leaguesList} data={random} renderItem={({item})=><CustomButton text='Serie A'/>} />
+          
+      </View>
+
+      <CustomButton text='Apagar Favoritos' />
+      
     </View>
   );
 }
@@ -23,9 +52,15 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
+  wrappers: {
     marginVertical: 30,
-    height: 1,
+    height:hp('20%') ,
     width: '80%',
+  },
+  leaguesList:{
+  
+    width: '100%',
+    alignItems:'flex-start',
+    justifyContent:'space-between',
   },
 });
